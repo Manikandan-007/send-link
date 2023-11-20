7c73db79aa7ba8cf2422efe2daf46afe95b258e1
Manikandan-007 <98259460+Manikandan-007@users.noreply.github.com>
2023-11-20 10:44:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+backend_link = 'https://your-backend-link.com'
+
+@app.route('/backend-link', methods=['GET'])
+def get_backend_link():
+    response = {
+        'backend_link': backend_link
+    }
+    return jsonify(response)
+
+@app.route('/set-backend-link',methods=['POST'])
+def set_backend_link():
+    backend_link = request.get_json().get('backend_link')
+    return jsonify({'status':'success'})
+
+if __name__ == '__main__':
+    app.run(debug=True) 
